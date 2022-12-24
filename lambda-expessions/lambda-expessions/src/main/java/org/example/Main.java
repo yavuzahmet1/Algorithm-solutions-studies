f58221146aa9d18a82713b263b610c3d5712a029
Coders club <1yavuzahmet@gmail.com>
2022-12-24 19:17:51 +0300

    lambda expressions firt excercises v2

--- lambda-expessions/lambda-expessions/src/main/java/org/example/Main.java
+++ lambda-expessions/lambda-expessions/src/main/java/org/example/Main.java
@@ -1,8 +1,21 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Lambda l=(x,y)->x*y;
-        System.out.println("result : "+l.run(5,7));
+        Lambda l = (x, y) -> x * y;
+        System.out.println("result : " + l.run(5, 7));
+        System.out.println("****");
+        List<Integer> intSeq = Arrays.asList(1, 2, 3, 4);
+        intSeq.forEach(x -> System.out.println(x));
+        System.out.println("****");
+        intSeq.forEach(y -> {
+            int z = y + 2;
+            System.out.println(z);
+        });
+        System.out.println("****");
+        intSeq.forEach(System.out::println);
     }
 }
