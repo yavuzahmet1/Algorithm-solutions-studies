b09bb31a1d007f8889409c767b8d71038f5ca805
Coders club <1yavuzahmet@gmail.com>
2023-01-20 23:37:39 +0300

    Contains Duplicate java solutions

--- leetcode/TechThursday-Graph/techThursday-graph/src/main/java/org/example/Graph.java
+++ leetcode/TechThursday-Graph/techThursday-graph/src/main/java/org/example/Graph.java
@@ -1,7 +1,16 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Graph {
-    private ArrayList<Integer> adj [];
+    private int V;
+    private ArrayList<Integer> graph [];
+    public Graph(int v){
+        V=v;
+        graph= new ArrayList[v];
+        for (int i=0;i<v;i++){
+            graph[i]=new LinkedList<>();
+        }
+    }
 }
